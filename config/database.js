7f6c12ea4131043f3acbafc52e53dfe871ba96ce
Ramachandra Reddy Maddigapu <ramachandrareddy.553@gmail.com>
2026-05-26 13:08:31 +0530

    Remove .env from git tracking

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
